6e343b2b22305e52245a25ec41e557fe49721966
Glenn Flåten <glennlfaten@gmail.com>
2022-01-11 10:34:14 +0100

    change

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ else
 fi
 
 # Make brew run in shell
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/t928390/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/<user>/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install XCode Command Line Tools
